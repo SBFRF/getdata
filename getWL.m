e60075ac4328d1c036c8c42c7518ed61cd819d13
Julia Fiedler <jfiedler@ucsd.edu>
2019-01-15 14:53:02 -0800

    updated url, variable names, and added functionality to hone in on netcdf file if time span is within the same month.

--- getWL.m
+++ getWL.m
@@ -7,40 +7,55 @@ function [WL] = getWL(d1, d2, gnum)
 %       gnum is the gauge number (11 only for WL)
 %           1 = gauge 11 (end of pier) - all that is currently available
 
-
-
+%TODO: Check input on time, if datenum or datetime format and proceed
+%accordingly. Optimal: datetime, probably.
 %
 % % % % % % % % 
 %% Main code 
 % defining data location url (1st part)
-svrloc='http://chlthredds.erdc.dren.mil/thredds/dodsC/frf';  % The prefix for the CHL thredds server
+svrloc='https://chlthredds.erdc.dren.mil/thredds/dodsC/frf';  % The prefix for the CHL thredds server
+
 % defining 2nd part
-if gnum==1;
-    urlback='oceanography/waterlevel/11/11.ncml'; % Water Level 
-elseif gnum==2;
-    urlback='oceanography/waterlevel/11/11.ncml'; 
+%TODO: Can change which gauge to point to here
+
+if gnum==1
+    urlback='/oceanography/waterlevel/eopNoaaTide'; % Water Level 
+elseif gnum==2
+    urlback='/oceanography/waterlevel/eopNoaaTide';    
 else
     disp 'please visit http://chlthredds.erdc.dren.mil/thredds/catalog/frf/catalog.html\n' ...
           'and browse to the gauge of interest and select proper url and add to program'
 end
-url=strcat(svrloc,urlback); % combining first and 2nd part of url string
 
+
+endUrl = '/eopNoaaTide.ncml';
+url=strcat(svrloc,urlback,endUrl); % combining first and 2nd part of url string
+
+d1vec = datevec(d1);
+d2vec = datevec(d2);
+
+if d1vec(1:2) == d2vec(1:2) %if the year + month are the same, download only that month
+    urlyear = num2str(d1vec(1));
+    urlmonth = num2str(d1vec(2));
+    url = [svrloc urlback '/' urlyear '/FRF-ocean_waterlevel_eopNoaaTide_' urlyear urlmonth '.nc'];
+end
+    
+%%
 % pulling down time now
 time=ncread(url,'time'); % downloading time from server
 tunit= ncreadatt(url,'time','units'); % reading attributes of variable time
-% converting time to matlab datetime
+% converting time to matlab datenum
 mtime=time/(3600.0*24)+datenum(1970,1,1);
 % finding index that corresponds to dates of interest
 sprintf('WL record starts %s and ends %s', datestr(min(mtime)),datestr(max(mtime)))
 itime=find(d1 < mtime & d2> mtime); % indicies in netCDF record of data of interest
 
-
 % pulling data from server with itime index 
 WL.time = mtime(itime); % record of time in matlab datetime 
-WL.WL = ncread(url,'waterLevelHeight',min(itime),length(itime));  % measured water level
-WL.PredictedWL = ncread(url,'predictedWaterLevelHeight',min(itime),length(itime));  % predicted WL
+WL.WL = ncread(url,'waterLevel',min(itime),length(itime));  % measured water level
+WL.PredictedWL = ncread(url,'predictedWaterLevel',min(itime),length(itime));  % predicted WL
 WL.name = ncread(url,'station_name'); % station name 
-WL.lat = ncread(url,'lat'); % latitutde 
-WL.lon = ncread(url, 'lon'); % lon 
+WL.lat = ncread(url,'latitude'); % latitutde 
+WL.lon = ncread(url, 'longitude'); % lon 
 
 end
